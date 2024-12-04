a62014a81cfa455285db383c3fea78c3ccd46c2e
AlPerBara <alperezba@unal.edu.co>
2024-12-04 13:32:32 -0500

    Update Zoologico.java

--- src/main/java/gestion/Zoologico.java
+++ src/main/java/gestion/Zoologico.java
@@ -50,7 +50,7 @@ public class Zoologico {
         zonas.add(zonaPorAgregar);
     }
 
-    public int cantidadTotalDeAnimales(){
+    public int cantidadTotalAnimales(){
         return Animal.getTotalAnimales();
 
     }
